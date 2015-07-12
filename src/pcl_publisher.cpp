f24cec864ab7d6f4d230d44bb459d5832bd5f7e5
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2015-07-12 13:51:55 -0700

    Publishing square-shaped point cloud region for ar tag. Still have problems with buffer overflow and visualizing the point cloud being updated live in rviz

--- src/pcl_publisher.cpp
+++ src/pcl_publisher.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
   PointCloud::Ptr msg(new PointCloud);
   msg->header.frame_id = "usb_cam";
   msg->height = 1;
-  msg->width = 100; // note: always make width the size of the total number of pts in ptcloud
+  msg->width = 2000; // note: always make width the size of the total number of pts in ptcloud
 
   tf::TransformListener transform_listener;
   
@@ -39,48 +39,21 @@ int main(int argc, char** argv)
       ros::Duration(1.0).sleep();
     }
 
-    // public point cloud with the size and shape of the VelociRoACH 
-    /*double centerX = transform.getOrigin().x();
-    double centerY = transform.getOrigin().y(); 
-    double centerZ = transform.getOrigin().z(); 
-
-    int arHeight = 7; // ar tag is 3 in or ~7.62 cm
-    int arWidth = 7;
-    for(int i=0; i < arHeight; i++){
-		for(int j=0;j < arWidth; j++){
-		
+	double x = 0.10;
+	while(x >= -0.10){
+		double y = -0.10;
+		while(y <= 0.10){
+			// get stamped pose wrt ar_marker
+			tf::Stamped<tf::Pose> corner_1(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(x, y, 0.0)),ros::Time(0), "ar_marker_2");
+			tf::Stamped<tf::Pose> transformed_corner_1;
+			// transform pose wrt usb_cam
+			transform_listener.transformPose("usb_cam", corner_1, transformed_corner_1);
+			// push xyz coord of pt to point cloud
+			msg->points.push_back (pcl::PointXYZ(transformed_corner_1.getOrigin().x(), transformed_corner_1.getOrigin().y(), transformed_corner_1.getOrigin().z()));
+			y += 0.05;
+		}
+		x -= 0.05;
 	}
-    }*/
-
-    double centerX = transform.getOrigin().x();
-    double centerY = transform.getOrigin().y(); 
-    double centerZ = transform.getOrigin().z();
-
-	tf::Stamped<tf::Pose> corner_1(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.10, -0.10, 0.0)),ros::Time(0), "ar_marker_2");
-	tf::Stamped<tf::Pose> corner_2(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.10, 0.10, 0.0)),ros::Time(0), "ar_marker_2");
-	tf::Stamped<tf::Pose> corner_3(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.10, -0.10, 0.0)), ros::Time(0), "ar_marker_2");
-	tf::Stamped<tf::Pose> corner_4(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.10, 0.10, 0.0)), ros::Time(0), "ar_marker_2");
-
-	tf::Stamped<tf::Pose> transformed_corner_1;
-	transform_listener.transformPose("usb_cam", corner_1, transformed_corner_1);
-	tf::Stamped<tf::Pose> transformed_corner_2;
-	transform_listener.transformPose("usb_cam", corner_2, transformed_corner_2);
-	tf::Stamped<tf::Pose> transformed_corner_3;
-	transform_listener.transformPose("usb_cam", corner_3, transformed_corner_3);
-	tf::Stamped<tf::Pose> transformed_corner_4;
-	transform_listener.transformPose("usb_cam", corner_4, transformed_corner_4);
-    
-	// push 4 corners of AR tag to pt cloud
-    msg->points.push_back (pcl::PointXYZ(transformed_corner_1.getOrigin().x(), transformed_corner_1.getOrigin().y(), transformed_corner_1.getOrigin().z()));
-
-	msg->points.push_back (pcl::PointXYZ(transformed_corner_2.getOrigin().x(), transformed_corner_2.getOrigin().y(), transformed_corner_2.getOrigin().z()));
-
-	msg->points.push_back (pcl::PointXYZ(transformed_corner_3.getOrigin().x(), transformed_corner_3.getOrigin().y(), transformed_corner_3.getOrigin().z()));
-
-	msg->points.push_back (pcl::PointXYZ(transformed_corner_4.getOrigin().x(), transformed_corner_4.getOrigin().y(), transformed_corner_4.getOrigin().z()));
-
-    // push origin of AR tag to pt cloud
-	msg->points.push_back (pcl::PointXYZ(centerX,centerY,centerZ));
 
     msg->header.stamp = ros::Time::now().toNSec();
     pub.publish(msg);
