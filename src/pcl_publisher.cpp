9e471d2885fa95bd99f7a70e829ee9c5c1409100
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2015-07-13 15:09:17 -0700

    fixed buffer overflow errors and generated xml file for ar bundle tracking for 3.5 x 3.5 x 3.5cm ar cube

--- src/pcl_publisher.cpp
+++ src/pcl_publisher.cpp
@@ -4,6 +4,10 @@
 #include <tf/transform_listener.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
@@ -19,37 +23,53 @@ int main(int argc, char** argv)
   PointCloud::Ptr msg(new PointCloud);
   msg->header.frame_id = "usb_cam";
   msg->height = 1;
-  msg->width = 2000; // note: always make width the size of the total number of pts in ptcloud
+  msg->width = 5000; // note: always make width the size of the total number of pts in ptcloud
+  //msg->is_dense = false;
+  msg->points.resize(msg->width * msg->height);
 
   tf::TransformListener transform_listener;
   
+  string ar_marker = "ar_marker_2";
+
+  int i = 0;
   ros::Rate loop_rate(4);
-  while (nh.ok())
+  while (nh.ok() && i < msg->points.size())
   {
     tf::StampedTransform transform;
     try{
       ros::Time now = ros::Time::now();
-      transform_listener.waitForTransform("usb_cam", "ar_marker_2", now, ros::Duration(3.0));
+      transform_listener.waitForTransform("usb_cam", ar_marker, now, ros::Duration(3.0));
 
-      // can't always guaruntee that it will be called ar_marker_2?
-      transform_listener.lookupTransform("usb_cam", "ar_marker_2", ros::Time(0), transform);
+	  cout << "Looking up tf from usb_cam to ar_marker...\n";
+      // can't always guaruntee that it will be called ar_marker?
+      transform_listener.lookupTransform("usb_cam", ar_marker, ros::Time(0), transform);
     }
     catch (tf::TransformException ex){
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
 
+    cout << "Found tf from usb_cam to ar_marker...\n";
 	double x = 0.10;
 	while(x >= -0.10){
 		double y = -0.10;
 		while(y <= 0.10){
 			// get stamped pose wrt ar_marker
-			tf::Stamped<tf::Pose> corner_1(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(x, y, 0.0)),ros::Time(0), "ar_marker_2");
-			tf::Stamped<tf::Pose> transformed_corner_1;
+			tf::Stamped<tf::Pose> corner(tf::Pose(tf::Quaternion(0, 0, 0, 1), tf::Vector3(x, y, 0.0)),ros::Time(0), ar_marker);
+			tf::Stamped<tf::Pose> transformed_corner;
 			// transform pose wrt usb_cam
-			transform_listener.transformPose("usb_cam", corner_1, transformed_corner_1);
+			transform_listener.transformPose("usb_cam", corner, transformed_corner);
 			// push xyz coord of pt to point cloud
-			msg->points.push_back (pcl::PointXYZ(transformed_corner_1.getOrigin().x(), transformed_corner_1.getOrigin().y(), transformed_corner_1.getOrigin().z()));
+			double tf_x = transformed_corner.getOrigin().x();
+			double tf_y = transformed_corner.getOrigin().y();
+			double tf_z = transformed_corner.getOrigin().z();
+			//msg->points.push_back (pcl::PointXYZ(tf_x, tf_y, tf_z));
+			msg->points[i].x = tf_x;
+			msg->points[i].y = tf_y;
+			msg->points[i].z = tf_z;
+			
+			i+=1;
+			cout << "Added (" << tf_x << "," << tf_y << "," << tf_z << ")\n";
 			y += 0.05;
 		}
 		x -= 0.05;
