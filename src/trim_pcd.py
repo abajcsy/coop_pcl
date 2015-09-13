019536b651bcaff75eeb27ae86708309fdd197a2
Austin Buchan <austin.d.buchan@gmail.com>
2015-09-13 09:42:22 -0700

    Added trim_pcd to remove 0s and large outliers

---
+++ src/trim_pcd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+
+if __name__ == '__main__':
+  pcd_file = sys.argv[1]
+  f = open(pcd_file,'r')
+  lines = f.readlines()
+  f.close()
+
+  for line in lines[:]:
+    vs = line.split()
+    if len(vs) == 3:
+      norm = sum([float(v)**2 for v in vs])**0.5
+      if norm == 0 or norm > 1.0:
+        lines.remove(line)
+
+  n_points = (len(lines)-11)
+  lines[6] = 'WIDTH %d\n' % n_points
+  lines[9] = 'POINTS %d\n' % n_points
+
+  f = open(pcd_file, 'w')
+  for line in lines:
+    f.write(line)
+  f.close()
